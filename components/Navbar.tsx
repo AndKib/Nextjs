0bdd6c574cf9e7d7241aa7be70123d65fc9d1bed
Andreas <andreas.kibsgaard@icloud.com>
2026-02-11 09:40:05 +0100

    Navbar med login, register og dashboard og home

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,43 @@
-import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "./ui/navigation-menu"
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "./ui/navigation-menu";
 
 function Navbar() {
   return (
     <NavigationMenu className="min-w-screen">
-        <NavigationMenuList>
-            <NavigationMenuItem>
-                <NavigationMenuLink>Login</NavigationMenuLink>
-            </NavigationMenuItem>
-        </NavigationMenuList>
-    </NavigationMenu >  
-  )
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link href="/">Home</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link href="/login">Login</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link href="/register">Register</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link href="/dashboard">Dashboard</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link href="/profile">Profile</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 }
 
-export default Navbar
+export default Navbar;
